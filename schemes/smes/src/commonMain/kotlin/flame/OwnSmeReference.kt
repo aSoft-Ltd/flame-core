dc16e79896a02d1a3086cc8d120e60c560909486
andylamax <andylamax@programmer.net>
2024-05-06 13:46:06 +0300

    finished working on document uploads

--- schemes/smes/src/commonMain/kotlin/flame/OwnSmeReference.kt
+++ schemes/smes/src/commonMain/kotlin/flame/OwnSmeReference.kt
@@ -2,16 +2,19 @@ package flame
 
 class OwnSmeReference(
     val load: () -> String,
+    val upload: (name: String) -> String,
     val update: () -> String,
 ) {
     companion object {
         fun messages() = OwnSmeReference(
             load = { "Loading sme info" },
+            upload = { "Uploading $it" },
             update = { "Updating your sme" },
         )
 
         fun endpoint(root: String) = OwnSmeReference(
             load = { "$root/sme/load" },
+            upload = { "$root/sme/document/$it" },
             update = { "$root/sme/update" },
         )
     }
